c7ff8db70e3180188074c996513b171db3f6535a
haleybristol <hebristol@gmail.com>
2016-01-22 14:23:25 -0800

    write third passing spec

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ var game = function(number) {
   var array = []
   for (var i = 1; i <= number; i++ ) {
     if ( i % 3 === 0) {
-        array.push("ping")
+      array.push("ping");
+    } else if ( i % 5 === 0) {
+      array.push("pong");
     } else {
       array.push(i);
 
