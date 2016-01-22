35ebac6186f047db4dd5f4c6bf06f74b6695dab2
haleybristol <hebristol@gmail.com>
2016-01-22 15:21:46 -0800

    adds user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,16 @@ var game = function(number) {
   }
     return array;
 };
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+      var number = parseInt($("input#number").val());
+      $("#result").empty();
+      var resultsArray = game(number);
+      resultsArray.forEach(function(i) {
+        $("#result").append("<li>" + i + "</li>");
+      });
+      $(".result").show();
+      event.preventDefault();
+  });
+});
